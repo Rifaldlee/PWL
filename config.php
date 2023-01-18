47b7a75326ffae103987856fbfb19f3b7939337a
johankevin2202@gmail.com <johankevin2202@gmail.com>
2023-01-18 16:52:16 +0700

    Login Revisi 1

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
     $db_host = 'localhost'; //server
     $db_user = 'root'; //username
     $db_pass = ''; //password
-    $db_name = 'rifaldlee1'; //nama db
+    $db_name = 'icehardwaredb'; //nama db
     $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name); //perintah untuk melakukan connect ke database sql
 
     if (!$conn) {
