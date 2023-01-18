0f91e5384da0699f00a90b269daf71335d627438
johankevin2202@gmail.com <johankevin2202@gmail.com>
2023-01-18 18:39:10 +0700

    login gaada yg berubah

--- edit.php
+++ edit.php
@@ -4,23 +4,23 @@ include("./config.php");
 // cek apa tombol daftar udah di klik blom
 if (isset($_POST['edit_data'])) {
     // ambil data dari form
-    $id = $_POST['edit_id'];
-    $nama = $_POST['edit_nama'];
-    $Pmasuk = $_POST['edit_Pmasuk'];
-    $jenis_barang = $_POST['edit_jenis_barang'];
-    $harga = $_POST['edit_harga'];
-    $tgl_masuk = $_POST['edit_tgl_masuk'];
+    $id = $row["id"];
+    $name = $_POST['name'];
+    $merk = $_POST['merk'];
+    $product_type = $_POST['product_type'];
+    $price = $_POST['price'];
+    $release_date = $_POST['release_date'];
 
 
 
     // query
-    $sql = "UPDATE barang SET nama='$nama', Pmasuk='$Pmasuk', jenis_barang='$jenis_barang', harga='$harga', tgl_masuk='$tgl_masuk' WHERE id = '$id'";
+    $sql = "UPDATE barang SET name='$name', merk='$merk', product_type='$product_type',price='$price', release_date='$release_date' WHERE id= '$id'";
     $query = mysqli_query($db, $sql);
 
     // cek apa query berhasil disimpan?
     if ($query)
-        header('Location: ./index.php?update=sukses');
+        header('Location: ./read.php?update=sukses');
     else
-        header('Location: ./index.php?update=gagal');
+        header('Location: ./read.php?update=gagal');
 } else
     die("Akses dilarang...");
